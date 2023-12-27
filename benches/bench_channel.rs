b7be59b689dc3d70e43f8a2eeb3dee8ff05c4fe6
root <2394626220@qq.com>
2023-12-27 22:27:52 +0800

    update more bench

--- benches/bench_channel.rs
+++ benches/bench_channel.rs
@@ -1,14 +1,39 @@
+use bench_crates::use_flume_mpmc;
 use bench_crates::use_flume_mpsc;
+use bench_crates::use_flume_spsc;
 use bench_crates::use_tokio_mpsc;
+use bench_crates::use_tokio_spsc;
+use bench_crates::DataProvider;
+use bench_crates::StringData;
+use bench_crates::U8Data;
 use criterion::{criterion_group, criterion_main, Criterion};
 
-pub fn flume_benchmark(c: &mut Criterion) {
-    c.bench_function("flume", |b| b.iter(use_flume_mpsc));
+fn get_name<T: DataProvider>(name: &str) -> String {
+    format!("{name}-{}", T::TYPE_LITERAL)
 }
 
-pub fn tokio_benchmark(c: &mut Criterion) {
-    c.bench_function("tokio", |b| b.iter(use_tokio_mpsc));
+pub fn flume_benchmark<T: DataProvider>(c: &mut Criterion) {
+    let name1 = get_name::<T>("flume-mpmc");
+    let name2 = get_name::<T>("flume-mpsc");
+    let name3 = get_name::<T>("flume-spsc");
+    c.bench_function(&name1, |b| b.iter(use_flume_mpmc::<T>));
+    c.bench_function(&name2, |b| b.iter(use_flume_mpsc::<T>));
+    c.bench_function(&name3, |b| b.iter(use_flume_spsc::<T>));
 }
 
-criterion_group!(benches, flume_benchmark, tokio_benchmark);
+pub fn tokio_benchmark<T: DataProvider>(c: &mut Criterion) {
+    let name1 = get_name::<T>("tokio-mpsc");
+    let name2 = get_name::<T>("tokio-spsc");
+    c.bench_function(&name1, |b| b.iter(use_tokio_mpsc::<T>));
+    c.bench_function(&name2, |b| b.iter(use_tokio_spsc::<T>));
+}
+
+criterion_group!(
+    benches,
+    flume_benchmark::<StringData>,
+    tokio_benchmark::<StringData>,
+    flume_benchmark::<U8Data>,
+    tokio_benchmark::<U8Data>
+);
+
 criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,68 @@
 use rand::{distributions::Alphanumeric, thread_rng, Rng};
 use tokio_util::task::TaskTracker;
 
-const RANDOM_DATA_LEN: usize = 20;
+const RANDOM_STRING_DATA_LEN: usize = 20;
 const CHAN_CAP: usize = 1000;
 const PRODUCER_NUM: usize = 10;
 
-fn gen_random_data(length: usize) -> String {
-    let rng = thread_rng();
-    let rand_string: String = rng
-        .sample_iter(&Alphanumeric)
-        .take(length)
-        .map(char::from)
-        .collect();
-    rand_string
+pub trait DataProvider {
+    type Type: Send + Clone + Unpin + 'static;
+    const TYPE_LITERAL: &'static str;
+
+    fn get_random_data() -> Self::Type;
 }
 
-pub async fn use_flume_mpsc() {
+pub struct StringData;
+
+impl DataProvider for StringData {
+    type Type = String;
+    const TYPE_LITERAL: &'static str = "string";
+
+    fn get_random_data() -> Self::Type {
+        let rand_string: String = thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(RANDOM_STRING_DATA_LEN)
+            .map(char::from)
+            .collect();
+        rand_string
+    }
+}
+
+pub struct U8Data;
+impl DataProvider for U8Data {
+    type Type = u8;
+    const TYPE_LITERAL: &'static str = "u8";
+    fn get_random_data() -> Self::Type {
+        todo!()
+    }
+}
+
+pub async fn use_flume_mpmc<T: DataProvider>() {
     let (tx, rx) = flume::bounded(CHAN_CAP);
-    let data = gen_random_data(RANDOM_DATA_LEN);
+    let data = T::get_random_data();
+    let tracer = TaskTracker::new();
+    (0..PRODUCER_NUM).for_each(|_| {
+        let tx = tx.clone();
+        let data = data.clone();
+        tracer.spawn(async move {
+            tx.send_async(data)
+                .await
+                .expect("send channel nerver fails");
+        });
+    });
+    (0..PRODUCER_NUM).for_each(|_| {
+        let rx = rx.clone();
+        tracer.spawn(async move {
+            _ = rx.recv_async().await.expect("recv channel nerver fails");
+        });
+    });
+    tracer.close();
+    tracer.wait().await;
+}
+
+pub async fn use_flume_mpsc<T: DataProvider>() {
+    let (tx, rx) = flume::bounded(CHAN_CAP);
+    let data = T::get_random_data();
     let tracer = TaskTracker::new();
     (0..PRODUCER_NUM).for_each(|_| {
         let tx = tx.clone();
@@ -35,9 +80,29 @@ pub async fn use_flume_mpsc() {
     tracer.wait().await;
 }
 
-pub async fn use_tokio_mpsc() {
+pub async fn use_flume_spsc<T: DataProvider>() {
+    let (tx, rx) = flume::bounded(CHAN_CAP);
+    let data = T::get_random_data();
+    let tracer = TaskTracker::new();
+    tracer.spawn(async move {
+        for _ in 0..PRODUCER_NUM {
+            let data = data.clone();
+            tx.send_async(data)
+                .await
+                .expect("send channel nerver fails");
+        }
+    });
+
+    for _ in 0..PRODUCER_NUM {
+        _ = rx.recv_async().await.expect("recv channel nerver fails");
+    }
+    tracer.close();
+    tracer.wait().await;
+}
+
+pub async fn use_tokio_mpsc<T: DataProvider>() {
     let (tx, mut rx) = tokio::sync::mpsc::channel(CHAN_CAP);
-    let data = gen_random_data(RANDOM_DATA_LEN);
+    let data = T::get_random_data();
     let tracer = TaskTracker::new();
     (0..PRODUCER_NUM).for_each(|_| {
         let tx = tx.clone();
@@ -46,6 +111,24 @@ pub async fn use_tokio_mpsc() {
             tx.send(data).await.expect("send channel nerver fails");
         });
     });
+    for _ in 0..PRODUCER_NUM {
+        _ = rx.recv().await.expect("recv channel nerver fails");
+    }
+    tracer.close();
+    tracer.wait().await;
+}
+
+pub async fn use_tokio_spsc<T: DataProvider>() {
+    let (tx, mut rx) = tokio::sync::mpsc::channel(CHAN_CAP);
+    let data = T::get_random_data();
+    let tracer = TaskTracker::new();
+    tracer.spawn(async move {
+        for _ in 0..PRODUCER_NUM {
+            let data = data.clone();
+            tx.send(data).await.expect("send channel nerver fails");
+        }
+    });
+
     for _ in 0..PRODUCER_NUM {
         _ = rx.recv().await.expect("recv channel nerver fails");
     }
